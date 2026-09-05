5002c6c9a9dcff9a74e4ec2c30e4beb731fe9304
bansiwalshubham5-hash <bansiwalshubham5@gmail.com>
2026-09-05 19:48:24 +0530

    Add moveWindowBy function to bubbleAPI

--- preload.js
+++ preload.js
@@ -3,4 +3,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('bubbleAPI', {
   togglePanel: () => ipcRenderer.send('toggle-panel'),
   showContextMenu: () => ipcRenderer.send('show-context-menu'),
+    moveWindowBy: (dx, dy) => ipcRenderer.send('move-bubble-by', dx, dy),
 });
